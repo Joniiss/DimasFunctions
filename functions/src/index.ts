739ff0c3b10b3d26c70ae1e2bce29336b5ccc2ba
João <joao.ferraz.balducci@gmail.com>
2024-09-21 18:57:51 -0300

    testev2 criada

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,15 +8,39 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+const app = admin.initializeApp();
+const db = app.firestore();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest(
+  {region: "southamerica-east1"},
+  (request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send(JSON.stringify(request));
+    functions.logger.debug(JSON.stringify(request));
+  }
+);
+
+exports.teste = functions
+  .region("southamerica-east1")
+  .https
+  .onCall((data, context) => {
+    functions.logger.debug(data);
+    return data;
+  });
+
+exports.testev2 = onRequest({region: "southamerica-east1"}, (req, res) => {
+  functions.logger.debug(req.body.s1);
+
+  db.collection("arduinos").add(req.body);
+
+  res.status(200).send("Recebido.");
+});
 
 
-// TESTE DO JONIS
+// firebase deploy --only functions:aaaa
